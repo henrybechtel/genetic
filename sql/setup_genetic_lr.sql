94257c7a13ff4500fb1ad68c865b1dc1359d8158
Henry Bechtel <hybechtel@gmail.com>
2024-08-29 21:38:26 -0700

    cleaning up and adding rmse ranking

--- sql/setup_genetic_lr.sql
+++ sql/setup_genetic_lr.sql
@@ -1,3 +1,7 @@
+-- Setting up initial tables for genentic linear regression
+
+CREATE SCHEMA IF NOT EXISTS genetic;
+
 SET search_path TO genetic;
 
 CREATE OR REPLACE FUNCTION normal_rand(mean DOUBLE PRECISION, stddev DOUBLE PRECISION)
@@ -24,3 +28,44 @@ BEGIN
     RETURN mean + z0 * stddev;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+
+drop table observations
+create table observations as(
+with vars as (
+    select 
+    3 as b0_true,
+    5 as b1_true
+)
+SELECT x,
+       b0_true + b1_true*x + normal_rand(0, 4) as y
+FROM
+    vars, 
+    (Select normal_rand(6, 3) AS x FROM generate_series(1, 20)) AS T
+)
+
+drop table if exists betas;
+create table betas as
+select 
+normal_rand(0, 3) AS b0,
+normal_rand(0, 3) AS b1
+FROM generate_series(1, 10)
+
+
+
+
+-- SELECT generate_series(
+--     '2024-01-01'::date,
+--     '2024-01-10'::date,
+--     '1 day'::interval
+-- );
+
+-- DROP TABLE IF EXISTS heteroscedastic;
+-- CREATE TABLE heteroscedastic AS
+-- SELECT x,
+--        x * normal_rand(0, 0.5) as y
+-- FROM
+--     (Select normal_rand(6, 3) AS x
+--      FROM generate_series(1, 100)) AS T
